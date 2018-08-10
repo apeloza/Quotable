f1694df78c5384fa2793c298cb36a00388384164
Tony <apeloza@alumni.uwo.ca>
2018-08-09 22:30:09 -0500

    woopdidy scoop

--- Core.lua
+++ Core.lua
@@ -1,6 +1,32 @@
 -- Create a new Ace3 module
-Quotable = LibStub("AceAddon-3.0"):NewAddon("Quotable", "AceConsole-3.0");
+Quotable = LibStub("AceAddon-3.0"):NewAddon("Quotable", "AceConsole-3.0", "AceEvent-3.0");
+
+local options = {
+    name = "Quotable",
+    handler = Quotable,
+    type = 'group',
+    args = {
+        speak = {
+            type = "execute",
+            name = "Speak",
+            desc = "Outputs a random quote.",
+            func = "Speak"
+        },
+    },
+}
 
 function Quotable:OnEnable()
-    Quotable:Print("Hello world!");
+    Quotable:Print("Welcome to Quotable! /quote or /quotable to use.");
+    LibStub("AceConfig-3.0"):RegisterOptionsTable("Quotable", options, {"quotable", "quote"})
+    Quotable.message = 'A test message';
+    Quotable.Speak();
+
+end
+
+function Quotable:Speak(info)
+    Quotable:Print('woopdidy scoop');
+end
+
+function Quotable:Save(info, newValue)
+    self.message = newValue
 end
