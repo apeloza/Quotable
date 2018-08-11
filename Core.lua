18553282d69589844d55c43f7ac001d6a352d3ee
Rom Stevens <romney.stevens@gmail.com>
2018-08-11 01:24:05 -0500

    Add dummy data, quote validation, and quote name entry

--- Core.lua
+++ Core.lua
@@ -17,7 +17,9 @@ local options = {
             name="Save",
             desc = "Saves a new quote to the database.",
             get="Save",
-            set="Save"
+            set="Save",
+            usage = "/quote save <name> <quote>",
+            validate = "ValidateQuote",
         },
         output = {
             type="input",
@@ -39,15 +41,42 @@ function Quotable:OnEnable()
     Quotable:Print("Welcome to Quotable! /quote or /quotable to use.");
     LibStub("AceConfig-3.0"):RegisterOptionsTable("Quotable", options, {"quotable", "quote"})
     -- declare defaults to be used in the DB
-
 end
 
 function Quotable:OnInitialize()
+    -- Database setup
     local defaults = {
         global = {
-            quotes = {},
             channel = 'PARTY',
-        },
+            quotes = {
+                {
+                    name = "Tigers",
+                    quote = "Oh no I thought we were going to the sandbox tigers",
+                    author = "Rom",
+                    date = 8/8/2018,
+                    tags = { "DMF" }
+                },
+                {
+                    name = "Loops",
+                    quote = "Brother may I have some loops",
+                    tags = { "meme" },
+                    date = 2018
+                },
+                {
+                    name = "Floor Pasta",
+                    quote = "Just serve them the floor pasta",
+                    author = "Tony",
+                    date = 8/9/2018,
+                    tags = { "Overcooked" }
+                },
+                {
+                    name = "Desolate Gays",
+                    quote = "Watch out for the gays",
+                    author = "Tony",
+                    tags = { "raid", "guild" }
+                }
+            },
+        }
     }
     -- Assuming the .toc says ## SavedVariables: QuotableDB
     Quotable.db = LibStub("AceDB-3.0"):New("QuotableDB", defaults, true);
@@ -64,11 +93,48 @@ end
 --Returns a random quote from the quote DB
 function Quotable:Random()
     local number = math.random(#Quotable.db.global.quotes);
-    return Quotable.db.global.quotes[number];
+    return Quotable.db.global.quotes[number].quote;
 end
 
-function Quotable:Save(info, newValue)
-    Quotable.db.global.quotes[#Quotable.db.global.quotes + 1] = newValue;
+function Quotable:Save(info, input)
+    local parsed_input = Quotable.ParseQuoteInput(info, input)
+    local name = parsed_input.name
+    Quotable.db.global.quotes[#Quotable.db.global.quotes + 1] = {
+        name = name,
+        quote = parsed_input.quote
+    }
+    Quotable:Print("Saved new quote with name: " .. name)
+end
+
+function Quotable:ValidateQuote(info, input)
+    local quote = Quotable.ParseQuoteInput(info, input) -- not sure why I need to pass info - it breaks if input isn't arg 2
+    if quote.status == "success" then
+        return true;
+    elseif quote.status == "error" then
+        return quote.message;
+    end
+end
+
+function Quotable:ParseQuoteInput(input)
+    local parsed_input = {
+        name = nil,
+        quote = nil,
+        message = "ok",
+        status = "success"
+    }
+
+    local parsed_name = string.match(input, "^(%S+) ")
+    local parsed_quote = string.match(input, (parsed_name or "") .. " (.+)$")
+
+    if parsed_name and parsed_quote then
+        parsed_input.name = parsed_name
+        parsed_input.quote = parsed_quote
+    else
+        parsed_input.status = "error"
+        parsed_input.message = "Name and quote are both required"
+    end
+
+    return parsed_input
 end
 
 function Quotable:SetOutput(info, newValue)
